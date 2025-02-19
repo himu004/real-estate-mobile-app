b4bb0d131ae20f82072f98db1b4625b6762c986a
Himu004 <himu.chowdhury01@gmail.com>
2025-02-19 19:20:55 +0600

    Real Estate Mobile Application Project Added

---
+++ app/signIn.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const signIn = () => {
+  return (
+    <View>
+      <Text>This is Sign in page</Text>
+    </View>
+  )
+}
+
+export default signIn
